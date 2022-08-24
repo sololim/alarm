dbaaf569d0a289c007d6e52f221b9684dcaf6f75
sololim <101944276+sololim@users.noreply.github.com>
2022-08-24 18:54:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.forever(function () {
+    music.playMelody("C5 C C5 C C5 C C5 C ", 120)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
